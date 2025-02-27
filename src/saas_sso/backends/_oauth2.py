640d64e12904d7686f15d16a9c53436c6c0215a7
Hsiaoming Yang <me@lepture.com>
2025-02-27 16:44:39 +0900

    fix: merge _util into _oauth2 file

--- src/saas_sso/backends/_oauth2.py
+++ src/saas_sso/backends/_oauth2.py
@@ -2,12 +2,13 @@ import typing as t
 import uuid
 import requests
 from abc import ABCMeta, abstractmethod
+from urllib import parse as urlparse
 from requests import PreparedRequest
 from requests.auth import AuthBase
 from joserfc import jwt
 from joserfc.jwk import KeySet
+from joserfc.util import to_bytes, to_str
 from django.core.cache import cache
-from ._util import add_params_to_qs, add_params_to_uri
 from .types import OAuth2Token, OAuth2Options, Placement, UserInfo
 
 __all__ = ["OAuth2Provider", "OAuth2Auth", "MismatchStateError"]
@@ -153,3 +154,27 @@ class OAuth2Provider(metaclass=ABCMeta):
     @abstractmethod
     def fetch_userinfo(self, token: OAuth2Token) -> UserInfo:
         pass
+
+
+def url_encode(params: t.Sequence[t.Tuple[t.Any, t.Any]]) -> str:
+    encoded = []
+    for k, v in params:
+        encoded.append((to_bytes(k), to_bytes(v)))
+    return to_str(urlparse.urlencode(encoded))
+
+
+def add_params_to_qs(query: str, params: t.Sequence[t.Tuple[str, str]]) -> str:
+    """Extend a query with a list of two-tuples."""
+    qs: t.List[t.Tuple[str, str]] = urlparse.parse_qsl(query, keep_blank_values=True)
+    qs.extend(params)
+    return url_encode(qs)
+
+
+def add_params_to_uri(uri: str, params: t.Sequence[t.Tuple[str, str]], fragment: bool = False) -> str:
+    """Add a list of two-tuples to the uri query components."""
+    sch, net, path, par, query, fra = urlparse.urlparse(uri)
+    if fragment:
+        fra = add_params_to_qs(fra, params)
+    else:
+        query = add_params_to_qs(query, params)
+    return urlparse.urlunparse((sch, net, path, par, query, fra))

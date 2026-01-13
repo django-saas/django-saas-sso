be21f633bc268fb63c8fe760ed33cbd010518a5e
Hsiaoming Yang <me@lepture.com>
2026-01-13 18:28:17 +0900

    chore: release 0.6.0

--- src/saas_sso/admin.py
+++ src/saas_sso/admin.py
@@ -4,5 +4,5 @@ from .models import UserIdentity
 
 @admin.register(UserIdentity)
 class UserIdentityAdmin(admin.ModelAdmin):
-    list_display = ['pk', 'subject', 'strategy', 'user', 'created_at']
+    list_display = ['subject', 'strategy', 'user', 'created_at']
     list_filter = ('strategy',)

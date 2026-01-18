2f85495c3c44606bdb169dbf66eafb006365e34a
Hsiaoming Yang <me@lepture.com>
2026-01-18 09:57:52 +0900

    fix: auto register saas_permissions

--- src/saas_sso/apps.py
+++ src/saas_sso/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class SingleSignOnConfig(AppConfig):
     name = 'saas_sso'
     verbose_name = 'SaaS SSO'
-
-    def ready(self):
-        import saas_sso.registry.default_perms  # noqa
